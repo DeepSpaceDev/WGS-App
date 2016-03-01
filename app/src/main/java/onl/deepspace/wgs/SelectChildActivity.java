4394bc816dfae467c1c4d7d53b28f7b982a878f6
Dennis Kugelmann <kugelmann.dennis@gmail.com>
2016-03-01 15:59:14 +0100

    New Layout

--- app/src/main/java/onl/deepspace/wgs/SelectChildActivity.java
+++ app/src/main/java/onl/deepspace/wgs/SelectChildActivity.java
@@ -2,13 +2,12 @@ package onl.deepspace.wgs;
 
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewParent;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
