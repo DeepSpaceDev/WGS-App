5b74bfb20fa0bf59d2bf0a68ae9531af44abd813
Dennis Kugelmann <kugelmann.dennis@gmail.com>
2016-09-12 14:27:30 +0200

    Remove unnecessary code from DialogFragment

--- app/src/main/java/onl/deepspace/wgs/Fragments/ColorPickerDialogFragment.java
+++ app/src/main/java/onl/deepspace/wgs/Fragments/ColorPickerDialogFragment.java
@@ -19,24 +19,9 @@ import onl.deepspace.wgs.activities.colorpicker.ColorPickerItem;
 import onl.deepspace.wgs.activities.colorpicker.ListViewColorPicker;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ColorPickerDialogFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ColorPickerDialogFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * A simple {@link DialogFragment} subclass.
  */
 public class ColorPickerDialogFragment extends DialogFragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
 
     public ColorPickerDialogFragment() {
         // Required empty public constructor
@@ -80,69 +65,18 @@ public class ColorPickerDialogFragment extends DialogFragment {
         return builder.create();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ColorPickerDialogFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ColorPickerDialogFragment newInstance(String param1, String param2) {
-        ColorPickerDialogFragment fragment = new ColorPickerDialogFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
     }
 }
